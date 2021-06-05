f707d1b0e51c5a7bd0961b8c0f5a49dac7de4ed3
Blake Ingenthron <blakeingenthron@gmail.com>
2021-06-05 11:37:14 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-    future: {
-        webpack5: true,
-      },
-    webpack: config => {
-      config.resolve.fallback = {
-        child_process: 'empty',
-        "crypto": false,
-        "http": false
-      };
-      return config;
-    },
-  };
+    webpack: (config) => {
+        config.node = {
+            child_process: "empty",
+    };
+    return config
+  },
+}
