8e0fad4920633a99257bcc3df6b1bd98615c360c
Blake Ingenthron <blakeingenthron@gmail.com>
2021-06-05 10:48:34 -0500

    testing solution

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    webpack: (config) => {
-        config.node = {
-        child_process: "empty",
-    };
-    return config
-    },
-  }
--- pages/api/auth/hooks.js
+++ pages/api/auth/hooks.js
@@ -1,9 +1,16 @@
 import { createUser } from 'utils/db'
-import { createCustomer } from 'utils/stripe'
+// import { createCustomer } from 'utils/stripe'
+import Stripe from 'stripe';
+const stripe = new Stripe((process.env.STRIPE_SECRET_KEY));
+
+const createCustomer = async (email) =>
+stripe.customers.create({
+  email,
+})
 
 module.exports = async (req, res) => {
   const { email, secret } = JSON.parse(req.body)
-
+ 
   if (secret === process.env.AUTH0_HOOK_SECRET) {
     try {
       console.log('creating user')
